a4bea66de6d9cacd34c53a359e55b9dfcad44b80
delgadosanty <s.delgado.perdomo@unillanos.edu.co>
2026-08-27 18:52:06 -0500

    Add abstract class CuentaBancaria with basic methods

---
+++ cuentabancaria.java
@@ -0,0 +1,55 @@
+public abstract class CuentaBancaria {
+
+
+
+private String titular;
+
+private double saldo;
+
+
+
+public CuentaBancaria(String titular, double saldo) {
+
+this.titular = titular;
+
+this.saldo = saldo;
+
+}
+
+
+
+public String getTitular() {
+
+return titular;
+
+}
+
+
+
+public void setTitular(String titular) {
+
+this.titular = titular;
+
+}
+
+
+
+public double getSaldo() {
+
+return saldo;
+
+}
+
+
+
+public void setSaldo(double saldo) {
+
+this.saldo = saldo;
+
+}
+
+
+
+public abstract void aplicarComisionMensual();
+
+}
